bf26a99af3faefe84665cf1244bb57c2793e6040
dhrumil4u360@gmail.com <dhrumil4u360@gmail.com>
2022-01-19 09:09:33 +0530

    initial version ready

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const { getDataset, getDatasetItem } = require('./helpers');
+const parsers = require('./parsers');
+
+async function retrieveDatasetItem(action, settings) {
+    const params = {
+        datasetId: parsers.string(action.params.datasetId),
+        offset: parseInt(action.params.offset || 0),
+        apifyToken: parsers.string(action.params.APIFY_TOKEN) || settings.APIFY_TOKEN
+    };
+    const datasetResponse = await getDataset(params);
+    if (datasetResponse.status !== 200) {
+        throw "Unable to connect to dataset";
+    }
+    if ((params.offset + 1) > datasetResponse.data.data.itemCount) {
+        throw `Invalid offset. Not enough records in dataset for offset ${params.offset}`;
+    }
+
+    const itemResponse = await getDatasetItem(params);
+
+    return {
+        offset: params.offset,
+        row: itemResponse.data[0],
+        total: datasetResponse.data.data.itemCount
+    };
+}
+
+module.exports = {
+    retrieveDatasetItem,
+};        
